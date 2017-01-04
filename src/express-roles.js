f20a1efada4627ba8a0840e324e1d3b58af7d3f8
Gerardo Gallegos <puxerop@gmail.com>
2017-01-03 20:56:17 -0600

    Was fixed the previous middleware logic

--- src/express-roles.js
+++ src/express-roles.js
@@ -13,31 +13,43 @@ function expressRoles(rolesConfig, prevMw) {
   // return the express middleware
   return (req, res, next)=> {
 
-    if(prevMw) {
-      prevMw(req, res, next)
+    const i_next = () => {
+      console.log('LLEGO HASTA ACA')
+      middleware()
     }
 
-    const errMsg = `Access Denied - You don\'t have permission to: ${req.route.path} - ${req.method.toLowerCase()}`
+    // If exist prevMw (prev middleware is executed before)
+    if(prevMw) {
+      return prevMw(req, res, i_next)
+    }
 
-    check_rol(rolesConfig, req)
-      // Validates that req.decode.rol be correct
-      .then( validRol => {
-        return check_route(validRol, req)
-      })
-      // Validates that req.route.path have a valid access
-      .then( validActions => {
-        return check_method(validActions, req)
-      })
-      // Validates that req.method have a valid access
-      .then(()=> {
-        // OK..
-        next()
-      })
-      .catch(err => {
-        res.status(401).json({
-          message: errMsg
+    middleware()
+
+    const errMsg = `Access Denied
+    You don't have permission to: ${req.route.path} - ${req.method.toLowerCase()}`
+
+    function middleware() {
+      check_rol(rolesConfig, req)
+        // Validates that req.decode.rol be correct
+        .then( validRol => {
+          return check_route(validRol, req)
         })
-      })
+        // Validates that req.route.path have a valid access
+        .then( validActions => {
+          return check_method(validActions, req)
+        })
+        // Validates that req.method have a valid access
+        .then(()=> {
+          // OK..
+          next()
+        })
+        .catch(err => {
+          res.status(401).json({
+            message: errMsg
+          })
+        })
+    }
+
   }
 }
 
